765604bd6ac40a166e640d756dc88a6af99aa899
mullanator216 <59973723+mullanator216@users.noreply.github.com>
2020-03-04 14:03:28 -0500

    Add files via upload

---
+++ IST-411-Spring-2020-Flappy-Bird/src/IST_411_Spring_2020_Flappy_Bird/Test.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IST_411_Spring_2020_Flappy_Bird;
+
+/**
+ *
+ * @author Jake_Gaming_PC
+ */
+public class Test 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("this is a test");
+    }
+   
+}
